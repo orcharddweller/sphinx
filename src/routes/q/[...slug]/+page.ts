2c8564bff3cdebe8b58399f3e7637a2058f81bc4
marcinplatek <maruplat@gmail.com>
2023-01-29 09:49:15 +0100

    feat: prerender setup

--- src/routes/q/[...slug]/+page.ts
+++ src/routes/q/[...slug]/+page.ts
@@ -2,6 +2,8 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { quizzes } from '$lib/config/content';
 
+export const prerender = true;
+
 /** @type {import('./$types').PageLoad} */
 export const load: PageLoad = ({ params }) => {
 	const quiz = quizzes[params.slug];
